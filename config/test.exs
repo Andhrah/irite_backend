e17921f19f31703b68944ceb1aa51c859aae151d
Alexandra Collins <andracolexa@gmail.com>
2019-11-17 11:54:06 +0100

    (continous-integration): set up circleci - add a conditional statement to config/test.exs for environment variable
    [Start #169657672]

--- config/test.exs
+++ config/test.exs
@@ -11,4 +11,11 @@ config :logger, level: :warn
 
 # Finally import the config/test.secret.exs which loads secrets
 # and configuration from environment variables.
-# import_config "test.secret.exs"
+if MIX_ENV === "test" do
+  config :irite, Irite.Repo,
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  database: System.get_env("POSTGRES_DB")
+else
+  import_config "test.secret.exs"
+end
