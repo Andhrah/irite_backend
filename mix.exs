393e99a46baaa7ed7bf92619bc5a5edddb7640e9
Alexandra Collins <andracolexa@gmail.com>
2019-11-17 18:01:28 +0100

    (coveralls): integrate coveralls - integrate coveralls for code coverage - install excoveralls for code coverage report - configure excoveralls in mix.exs
    [Start #169657892]

--- mix.exs
+++ mix.exs
@@ -10,7 +10,8 @@ defmodule Irite.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -40,7 +41,8 @@ defmodule Irite.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:excoveralls, "~> 0.12.0", only: :test}
     ]
   end
 
