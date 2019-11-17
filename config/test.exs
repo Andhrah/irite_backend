fcfd69341b551aa3222e2d1c129841491189db52
Alexandra Collins <andracolexa@gmail.com>
2019-11-17 16:15:19 +0100

    (continous-intergration): setup circleci - modify .gitignore file - modify config/dev.exs file - modify config/test.exs file - delete config/test.secret.exs file - delete config/dev.secret.exs file
    [Finish #169657672]

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,13 @@
 use Mix.Config
 
+# Configure your database
+# change the password to your local postgres password
+config :irite, Irite.Repo,
+username: "postgres" || System.get_env("POSTGRES_USER"), 
+password: "postgres" || System.get_env("POSTGRES_PASSWORD"),
+database: "irite_test" || System.get_env("POSTGRES_DB"),
+pool: Ecto.Adapters.SQL.Sandbox
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :irite, IriteWeb.Endpoint,
@@ -8,21 +16,3 @@ config :irite, IriteWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-
-# Finally import the config/test.secret.exs which loads secrets
-# and configuration from environment variables.
-# IO.puts("First one, I'm working")
-# defmodule Env do
-  # def env do
-    # if Mix.env === :test do
-    #   IO.puts("I'm working")
-      config :irite, Irite.Repo,
-      username: System.get_env("POSTGRES_USER") ,
-      password: System.get_env("POSTGRES_PASSWORD"),
-      database: System.get_env("POSTGRES_DB"),
-      pool: Ecto.Adapters.SQL.Sandbox
-    # else
-    #   import_config "test.secret.exs"
-    # end
-  # end
-# end
